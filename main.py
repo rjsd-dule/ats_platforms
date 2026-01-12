ff850e0c6db5df8ca918a92ab950e77dee0c95be
rjsd-dule <rumijsd27@gmail.com>
2026-01-12 13:47:23 -0500

    Update file main

---
+++ main.py
@@ -0,0 +1,6 @@
+import json
+from .services.cv_processor import CVProcessor
+from .services.utilities_and_validation import validate_and_extract, handle_analysis_error
+
+
+#4full_text,num_pages = validate_and_extract()
