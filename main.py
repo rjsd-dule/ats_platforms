00f4a9dac71b36ea86f079c6a808dc14a674f441
rjsd-dule <rumijsd27@gmail.com>
2026-01-13 23:44:39 -0500

    Update

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from .services.cv_processor import CVProcessor
 from .services.utilities_and_validation import validate_and_extract, handle_analysis_error
 
 
-#4full_text,num_pages = validate_and_extract()
+full_text,num_pages = validate_and_extract()
+
